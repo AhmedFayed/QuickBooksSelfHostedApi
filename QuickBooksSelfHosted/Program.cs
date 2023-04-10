b9aec817224978403d89141516c8448ab00fba98
AhmedFayed <ahmed.hamdy1021994@gmail.com>
2023-04-10 13:14:13 +0200

    Remove Windows Service Code

--- QuickBooksSelfHosted/Program.cs
+++ QuickBooksSelfHosted/Program.cs
@@ -10,66 +10,21 @@ namespace QuickBooksSelfHostedApi
     class Program
     {
 
-        #region Nested classes to support running as service
-        public const string ServiceName = "QuickBooksCommunicationService";
-
-        public class Service : ServiceBase
-        {
-            public Service()
-            {
-                ServiceName = Program.ServiceName;
-            }
-
-            protected override void OnStart(string[] args)
-            {
-                Program.Start(args);
-            }
-
-            protected override void OnStop()
-            {
-                Program.Stop();
-            }
-        }
-        #endregion
-
         static void Main(string[] args)
-        { 
-            if (!Environment.UserInteractive)
-                // running as service
-                using (var service = new Service())
-                    ServiceBase.Run(service);
-            else
-            {
-                // running as console app
-                Start(args);
-                Console.WriteLine("Press any key to stop...");
-                Console.ReadKey(true);
-                Stop();
-            }
-
-        }
-
-        private static void Start(string[] args)
         {
-            
-
             var port = ConfigurationReader.GetValue("Port");
             var startOptions = new StartOptions(url: $"http://*:{port}/");
 
-            Console.WriteLine($"Service Started Successfully On Port: {port}...");
             // Start OWIN host 
             using (WebApp.Start<Startup>(startOptions))
             {
+                Console.WriteLine($"Service Started Successfully On Port: {port}...");
                 while (true)
                 {
                     Console.ReadLine();
                 }
             }
-        }
 
-        private static void Stop()
-        {
-            // onstop code here
         }
     }
 }
